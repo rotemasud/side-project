65a29a77ec81becf8c7c838a8df478c3e1e39c4e
rotemasud <rotemasud@gmail.com>
2025-02-08 19:10:38 +0200

    add karpenter installation script

---
+++ install_karpenter.sh
@@ -0,0 +1,16 @@
+export KARPENTER_NAMESPACE="kube-system"
+export KARPENTER_VERSION="1.0.8"
+export K8S_VERSION="1.31"
+export CLUSTER_NAME="side-project-cluster"
+
+# Logout of helm registry to perform an unauthenticated pull against the public ECR
+helm registry logout public.ecr.aws
+
+helm upgrade --install karpenter oci://public.ecr.aws/karpenter/karpenter --version "${KARPENTER_VERSION}" --namespace "${KARPENTER_NAMESPACE}" --create-namespace \
+  --set "settings.clusterName=${CLUSTER_NAME}" \
+  --set "settings.interruptionQueue=${CLUSTER_NAME}" \
+  --set controller.resources.requests.cpu=1 \
+  --set controller.resources.requests.memory=1Gi \
+  --set controller.resources.limits.cpu=1 \
+  --set controller.resources.limits.memory=1Gi \
+  --wait
