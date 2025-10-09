8b6357f6a3c01ae25367408f388983ca7df07d95
Rotem Masoud <rotemm@Mac.lan>
2025-10-09 16:18:49 +0300

    add support for argo rollouts and align versions across docker image and spring application version

--- src/main/java/com/example/side_project/HelloController.java
+++ src/main/java/com/example/side_project/HelloController.java
@@ -1,10 +1,14 @@
 package com.example.side_project;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+	@Value("${app.version}")
+	private String appVersion;
 
 	@GetMapping("/")
 	public String index() {
@@ -15,7 +19,7 @@ public class HelloController {
 		//System.out.println(response.asString());
 		//client.close(); // Closes the internal resources used by the Ec2MetadataClient class.
 
-		return "Greetings from Spring Boot!";
+		return "Greetings from Spring Boot! - Version " + appVersion;
 	}
 
 }
